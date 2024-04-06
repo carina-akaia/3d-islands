2bbebf907062d6f99bcdfe783b1f85f9270ad398
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-04-06 05:27:58 +0400

    🚧 wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={inter.className} style={{ width: "100dvw", height: "100dvh" }}>
 				<Suspense fallback={null}>{children}</Suspense>
 			</body>
 		</html>
